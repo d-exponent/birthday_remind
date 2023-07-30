ce4aba01d4467d87571b5f22fa7fad4bd98b0e9a
Desmond Odion <106418626+d-exponent@users.noreply.github.com>
2023-07-30 18:22:11 +0100

    move server to https://github.com/d-exponent/birthday_reminder_server.git

--- Server/src/routes/api-v1.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-
-router.use('/users', require('./users'))
-router.use('/birthdays', require('./birthdays'))
-router.use('/auth', require('./auth'))
-
-module.exports = router
--- Server/src/utils/app-error.js
+++
@@ -1,8 +0,0 @@
-module.exports = class AppError extends Error {
-  constructor(message, status) {
-    super(message)
-    this.status = status
-    this.isOperational = true
-    Error.captureStackTrace(this, this.constructor)
-  }
-}
